9e112d134f3e703e8861ee1617a05278833f2add
er-ayushmittal <60571990+er-ayushmittal@users.noreply.github.com>
2021-10-10 22:32:47 +0530

    assignment9

---
+++ build/precache-manifest.6273110a095f507c7a0af92cf5cabcf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85feed8a087636f320646ca66ea6f694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7ce369402cd78e8aac3",
+    "url": "/static/css/main.273b621a.chunk.css"
+  },
+  {
+    "revision": "8918d0af2062251e56c3",
+    "url": "/static/js/2.fb5ada59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb5ada59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ce369402cd78e8aac3",
+    "url": "/static/js/main.17c6016f.chunk.js"
+  },
+  {
+    "revision": "eb323c622e106faecb0e",
+    "url": "/static/js/runtime-main.341c860d.js"
+  },
+  {
+    "revision": "4f646371b2a2501c7acd9110625993ca",
+    "url": "/static/media/background.4f646371.jpg"
+  }
+]);
